62fdfd95cfc2eda9f95e86a7e2fa9062cfb67d3b
Matheus Moreira <matheusm920@gmail.com>
2020-07-29 12:03:08 -0300

    Realizando primeiro teste

---
+++ Teste1/README.md
@@ -0,0 +1,19 @@
+# Teste 1: WebScraping
+
+Para o primeiro teste foi escolhido utilizar a linguagem Node.js em conjunto com o poderoso módulo Puppeteer.js, que permite rodar um browser Chromium headless, de modo a automatizar o fluxo de download do PDF solicitado.
+
+## Pré-requisitos
+
+É necessário ter Node.js e uma conexão com a internet para executar esse teste.
+
+## Usagem
+
+Basta clonar o projeto e então dirigir-se até a pasta Teste1. A partir daí é necessário rodar:
+
+`npm i`
+
+Depois basta rodar:
+
+`node index.js`
+
+E o arquivo "ComponenteOrganizacional.pdf" estará disponível na raiz de Teste1.
---
+++ Teste1/index.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const http = require('http');
+const fs = require('fs');
+
+(async () => {
+    // Iniciando o browser do Puppeteer e criando uma nova página
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Acessando a página solicitada no teste
+    await page.goto('http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar');
+    // Clicando no link mais recente
+    await page.click('a.alert-link');
+    // Recuperando o link para o PDF do componente organizacional
+    const urlToDownload = await page.$eval('a.btn.center-block', element => element.href);
+    await browser.close();
+
+    // Agora é feito o download do arquivo presente no link
+    const file = fs.createWriteStream('ComponenteOrganizacional.pdf');
+    http.get(urlToDownload, response => {
+        // Em caso de sucesso o evento 'finish' é disparado e uma mensagem é escrita ao usuário
+        response.pipe(file);
+        file.on('finish', () => {
+            file.close(() => {
+                console.log('Arquivo baixado com sucesso!');
+                process.exit(0);
+            });
+        });
+    }).on('error', err => {
+        // Em caso de algum erro o arquivo é apagado e uma mensagem é escrita ao usuário informando o erro
+        fs.unlink('ComponenteOrganizacional.pdf', () => {
+            console.log(`Erro ao gravar o arquivo: ${err.message}`)
+            process.exit(1);
+        })
+    })
+})();
